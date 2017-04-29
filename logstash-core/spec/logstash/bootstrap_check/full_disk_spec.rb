a42a7ffab46eb358637df466b89b88f16cb9c1f3
Armin Braun <me@obrown.io>
2017-04-29 18:14:37 +0200

    #6552 rspec test

--- logstash-core/spec/logstash/bootstrap_check/full_disk_spec.rb
+++ logstash-core/spec/logstash/bootstrap_check/full_disk_spec.rb
@@ -6,19 +6,20 @@ describe LogStash::BootstrapCheck::FullDisk do
 
   context("when persisted queues are enabled") do
     let(:settings) do
-      settings = LogStash::Settings.new
-      settings.set("queue.type", "persisted")
-      settings.set("path.queue", ::File.join(Dir.tmpdir, "some/path"))
+      settings = LogStash::SETTINGS.dup
+      settings.set_value("queue.type", "persisted")
+      settings.set_value("path.queue", ::File.join(Dir.tmpdir, "some/path"))
+      settings
     end
 
     it "should not throw if enough space available" do
-      settings.set("queue.page_capacity", 1024)
-      expect(subject.check(settings)).to_not raise_error
+      settings.set_value("queue.page_capacity", 1024)
+      expect { LogStash::BootstrapCheck::FullDisk.check(settings) }.to_not raise_error
     end
 
     it "should throw if not enough space available" do
-      settings.set("queue.page_capacity", 1024 ** 5)
-      expect(subject.check(settings)).to raise_error
+      settings.set_value("queue.page_capacity", 1024 ** 5)
+      expect { LogStash::BootstrapCheck::FullDisk.check(settings) }.to raise_error
     end
   end
 end
