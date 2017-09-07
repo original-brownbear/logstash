5f813d30af0967f2c410b62b89dcdd2baa17c38e
Armin <me@obrown.io>
2017-09-07 13:29:38 +0200

    step 1

--- logstash-core/src/main/java/org/logstash/RubyUtil.java
+++ logstash-core/src/main/java/org/logstash/RubyUtil.java
@@ -1,6 +1,12 @@
 package org.logstash;
 
+import java.io.IOException;
 import org.jruby.Ruby;
+import org.logstash.ackedqueue.ext.JrubyAckedBatchExtLibrary;
+import org.logstash.ackedqueue.ext.JrubyAckedQueueExtLibrary;
+import org.logstash.ackedqueue.ext.JrubyAckedQueueMemoryExtLibrary;
+import org.logstash.ext.JrubyEventExtLibrary;
+import org.logstash.ext.JrubyTimestampExtLibrary;
 
 /**
  * Utilities around interaction with the {@link Ruby} runtime.
@@ -28,6 +34,15 @@ public final class RubyUtil {
     private static Ruby setupRuby() {
         final Ruby ruby = Ruby.getGlobalRuntime();
         ruby.getOrCreateModule(LS_MODULE_NAME);
+        try {
+            new JrubyAckedQueueExtLibrary().load(ruby, false);
+            new JrubyAckedQueueMemoryExtLibrary().load(ruby, false);
+            new JrubyTimestampExtLibrary().load(ruby, false);
+            new JrubyAckedBatchExtLibrary().load(ruby, false);
+            new JrubyEventExtLibrary().load(ruby, false);
+        } catch (final IOException ex) {
+            throw new IllegalStateException(ex);
+        }
         return ruby;
     }
 }
