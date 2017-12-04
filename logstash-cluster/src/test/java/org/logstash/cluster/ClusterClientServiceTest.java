0447aaba7cc36dfb6e76a120d0c106445a53902f
Armin <me@obrown.io>
2017-12-04 10:58:46 +0100

    dirty

---
+++ logstash-cluster/src/test/java/org/logstash/cluster/ClusterClientServiceTest.java
@@ -0,0 +1,10 @@
+package org.logstash.cluster;
+
+import org.elasticsearch.test.ESTestCase;
+
+public final class ClusterClientServiceTest extends ESTestCase {
+
+    public void discoveryTest() {
+
+    }
+}
