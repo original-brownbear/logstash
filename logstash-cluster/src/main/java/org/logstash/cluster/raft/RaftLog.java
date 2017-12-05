fc748040a2db6555aaff88bdce20ab6e240a55f0
Armin <me@obrown.io>
2017-12-05 20:18:49 +0100

    bck

--- logstash-cluster/src/main/java/org/logstash/cluster/raft/RaftLog.java
+++ logstash-cluster/src/main/java/org/logstash/cluster/raft/RaftLog.java
@@ -1,21 +1,40 @@
 package org.logstash.cluster.raft;
 
 import java.io.Closeable;
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
+import org.jetbrains.annotations.NotNull;
+import org.mapdb.DB;
+import org.mapdb.DataInput2;
+import org.mapdb.DataOutput2;
+import org.mapdb.Serializer;
 
 public final class RaftLog implements Closeable {
 
-    private final List<RaftLogEntry> entries = new ArrayList<>();
+    private final List<RaftLogEntry> entries;
 
-    public RaftLog() {
+    private final DB database;
+
+    public RaftLog(final DB database) {
+        this.database = database;
+        this.entries = database.indexTreeList("raftLog", new Serializer<RaftLogEntry>() {
+            @Override
+            public void serialize(@NotNull final DataOutput2 out, @NotNull final RaftLogEntry value)
+                throws IOException {
+
+            }
+
+            @Override
+            public RaftLogEntry deserialize(@NotNull final DataInput2 input, final int available)
+                throws IOException {
+                return null;
+            }
+        }).createOrOpen();
     }
 
     public RaftLogEntry get(final long index) {
-        if ((long) entries.size() < index + 1L) {
-            throw new IllegalStateException(
-                String.format("Don't have an entry at index %d.", index)
-            );
+        if (index >= Integer.MAX_VALUE) {
+            throw new IllegalArgumentException(String.format("Index %d out of bounds", index));
         }
         return entries.get((int) index);
     }
@@ -26,5 +45,7 @@ public final class RaftLog implements Closeable {
 
     @Override
     public void close() {
+        database.commit();
+        database.close();
     }
 }
