e4a75f0b890e9dae38bd77af8e4fb4b28a99adcf
Armin <me@obrown.io>
2017-12-07 18:59:44 +0100

    bck

--- logstash-core/src/test/java/org/logstash/plugins/input/ClusterInputTest.java
+++ logstash-core/src/test/java/org/logstash/plugins/input/ClusterInputTest.java
@@ -3,18 +3,17 @@ package org.logstash.plugins.input;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.Collections;
-import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.LinkedTransferQueue;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.logstash.RubyUtil;
 import org.logstash.TestUtils;
-import org.logstash.cluster.LogstashCluster;
 import org.logstash.cluster.LogstashClusterConfig;
+import org.logstash.cluster.LogstashClusterServer;
 import org.logstash.cluster.primitives.queue.WorkQueue;
 import org.logstash.cluster.serializer.Serializer;
 import org.logstash.ext.JrubyEventExtLibrary;
@@ -31,9 +30,9 @@ public final class ClusterInputTest {
     @Rule
     public final TemporaryFolder temporaryFolder = new TemporaryFolder();
 
-    @Test
+    @Test(timeout = 15_000L)
     public void testSimpleTask() throws Exception {
-        final BlockingQueue<JrubyEventExtLibrary.RubyEvent> queue = new ArrayBlockingQueue<>(1);
+        final BlockingQueue<JrubyEventExtLibrary.RubyEvent> queue = new LinkedTransferQueue<>();
         final ExecutorService exec = Executors.newSingleThreadScheduledExecutor();
         final LogstashClusterConfig config = new LogstashClusterConfig(
             new InetSocketAddress(InetAddress.getLoopbackAddress(), TestUtils.freePort()),
@@ -52,31 +51,25 @@ public final class ClusterInputTest {
                  )
         ) {
             exec.execute(input);
-            LogstashCluster cluster = null;
+            LogstashClusterServer cluster = null;
             try {
                 cluster = getClient(
                     new LogstashClusterConfig(
-                        new InetSocketAddress(
-                            InetAddress.getLoopbackAddress(),
-                            TestUtils.freePort()
-                        ),
+                        new InetSocketAddress(InetAddress.getLoopbackAddress(), TestUtils.freePort()),
                         Collections.singleton(config.localNode()),
                         temporaryFolder.newFolder()
                     )
                 );
-                assertThat(cluster.getWorkQueueNames(), contains("logstashWorkQueue"));
+                assertThat(cluster.getWorkQueueNames(), contains(ClusterInput.P2P_QUEUE_NAME));
                 final WorkQueue<EnqueueEvent> tasks =
-                    cluster.<EnqueueEvent>workQueueBuilder().withName("logstashWorkQueue")
+                    cluster.<EnqueueEvent>workQueueBuilder().withName(ClusterInput.P2P_QUEUE_NAME)
                         .withSerializer(Serializer.JAVA).build();
                 tasks.addOne(
                     events -> events.push(
                         new JrubyEventExtLibrary.RubyEvent(RubyUtil.RUBY, RubyUtil.RUBY_EVENT_CLASS)
                     )
                 );
-                assertThat(
-                    queue.poll(10L, TimeUnit.SECONDS),
-                    instanceOf(JrubyEventExtLibrary.RubyEvent.class)
-                );
+                assertThat(queue.take(), instanceOf(JrubyEventExtLibrary.RubyEvent.class));
             } finally {
                 if (cluster != null) {
                     cluster.close().join();
@@ -85,8 +78,8 @@ public final class ClusterInputTest {
         }
     }
 
-    private static LogstashCluster getClient(final LogstashClusterConfig config) {
-        return LogstashCluster.builder().withLocalNode(config.localNode())
+    private static LogstashClusterServer getClient(final LogstashClusterConfig config) {
+        return LogstashClusterServer.builder().withLocalNode(config.localNode())
             .withBootstrapNodes(config.getBootstrap())
             .withDataDir(config.getDataDir())
             .withNumPartitions(1)
