5900c4f986c5bc50f7533f101a0a381b628aa525
Armin <me@obrown.io>
2017-12-07 23:02:55 +0100

    logs fixed

--- logstash-core/lib/logstash/cluster/logstash-input-cluster.rb
+++ logstash-core/lib/logstash/cluster/logstash-input-cluster.rb
@@ -5,21 +5,27 @@ require "logstash/namespace"
 class LogStash::Inputs::Cluster < LogStash::Inputs::Base
   config_name "cluster"
 
-  config :port, :validate => :number, :default => 8099
+  config :bind_port, :validate => :number, :default => 8099
 
-  config :host, :validate => :string, :default => "127.0.0.1"
+  config :bind_host, :validate => :string, :default => "127.0.0.1"
+
+  config :node_id, :validate => :string, :default => "node1"
+
+  config :es_host, :validate => :string, :default => "localhost:9200"
+
+  config :es_index, :validate => :string, :default => "lscluster"
+
+  config :data_path, :validate => :string, :default => "/tmp/lsqueue-data"
 
   def register
 
   end
 
-  # def register
-
   def run(queue)
     @wrapped_queue = org.logstash.plugins.input.ClusterInput.new(
         queue, org.logstash.cluster.LogstashClusterConfig.new(
-        java.net.InetSocketAddress.new(host, port), java.util.Collections.empty_list,
-        java.io.File.new("/tmp/lsqueue-data"))
+        node_id, java.net.InetSocketAddress.new(bind_host, bind_port),
+        java.util.Collections.empty_list, java.io.File.new(data_path))
     )
     @wrapped_queue.run
   end
