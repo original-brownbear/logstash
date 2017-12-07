13908f3bfac0048f5a3471c0fa309c179b029ec0
Armin <me@obrown.io>
2017-12-07 21:48:41 +0100

    logs fixed

--- logstash-core/lib/logstash/cluster/logstash-input-cluster.rb
+++ logstash-core/lib/logstash/cluster/logstash-input-cluster.rb
@@ -5,6 +5,8 @@ require "logstash/namespace"
 class LogStash::Inputs::Cluster < LogStash::Inputs::Threadable
   config_name "cluster"
 
+  config :port, :validate => :number, :default => 8099
+
   def register
 
   end
@@ -12,10 +14,15 @@ class LogStash::Inputs::Cluster < LogStash::Inputs::Threadable
   # def register
 
   def run(queue)
-
+    @wrapped_queue = org.logstash.plugins.input.ClusterInput.new(
+        queue, org.logstash.cluster.LogstashClusterConfig.new(
+        java.net.InetSocketAddress.new("127.0.0.1", 8099), java.util.Collections.empty_list,
+        java.io.File.new("/tmp/data"))
+    )
+    @wrapped_queue.run
   end
 
   def close
-
+    @wrapped_queue.close
   end
 end
