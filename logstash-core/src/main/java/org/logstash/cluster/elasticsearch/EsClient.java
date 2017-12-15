0fb30d4386a835d49b2e50bcb7929c3715073526
Armin <me@obrown.io>
2017-12-15 09:31:56 +0100

    bck

--- logstash-core/src/main/java/org/logstash/cluster/elasticsearch/EsClient.java
+++ logstash-core/src/main/java/org/logstash/cluster/elasticsearch/EsClient.java
@@ -11,6 +11,7 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.client.Client;
 import org.logstash.cluster.cluster.Node;
 import org.logstash.cluster.cluster.NodeId;
@@ -35,24 +36,23 @@ public final class EsClient implements Closeable {
 
     private final Node localNode;
 
-    public EsClient(final Client client, final String index, final Node localNode) {
+    public static EsClient create(final Client client, final String index, final Node localNode)
+        throws ExecutionException, InterruptedException {
+        return new EsClient(client, index, localNode);
+    }
+
+    private EsClient(final Client client, final String index, final Node localNode)
+        throws ExecutionException, InterruptedException {
         this.client = client;
         this.index = index;
         this.localNode = localNode;
+        ensureIndex();
     }
 
     public Collection<Node> loadBootstrap() {
         try {
             ensureIndex();
-            final Collection<Object> nodes = (Collection<Object>) client.prepareGet()
-                .setIndex(index)
-                .setType(ES_TYPE)
-                .setId(ES_BOOTSTRAP_DOC).setRealtime(true).setRefresh(true)
-                .execute().actionGet()
-                .getSource()
-                .get(ES_NODES_FIELD);
-            return nodes.stream().map(String.class::cast).map(EsClient::deserializeNode)
-                .collect(Collectors.toList());
+            return deserializeNodesResponse(getNodesResponse());
         } catch (final InterruptedException | ExecutionException ex) {
             throw new IllegalStateException(ex);
         }
@@ -61,25 +61,44 @@ public final class EsClient implements Closeable {
     public void saveBootstrap(final Collection<Node> nodes)
         throws ExecutionException, InterruptedException {
         LOGGER.info("Saving updated bootstrap node list to Elasticsearch.");
+        final GetResponse existing = getNodesResponse();
         final Collection<Node> update = new HashSet<>(nodes);
+        if (existing.isExists()) {
+            update.addAll(deserializeNodesResponse(existing));
+        }
         update.add(localNode);
         final Collection<String> serialized = update.stream().map(EsClient::serializeNode)
             .collect(Collectors.toList());
-        client.prepareUpdate().setId(ES_BOOTSTRAP_DOC)
-            .setDocAsUpsert(true).setDoc(Collections.singletonMap(ES_NODES_FIELD, serialized))
-            .setIndex(index).setType(ES_TYPE)
-            .setUpsert(Collections.singletonMap(ES_NODES_FIELD, serialized)).execute().get();
+        client.prepareUpdate().setId(ES_BOOTSTRAP_DOC).setDoc(
+            Collections.singletonMap(ES_NODES_FIELD, serialized)
+        ).setIndex(index).setType(ES_TYPE).setDocAsUpsert(true).setUpsert(
+            Collections.singletonMap(ES_NODES_FIELD, serialized)
+        ).execute().get();
     }
 
     @Override
     public void close() {
     }
 
+    private static Collection<Node> deserializeNodesResponse(final GetResponse response) {
+        return ((Collection<String>) response.getSource().get(ES_NODES_FIELD))
+            .stream().map(EsClient::deserializeNode)
+            .collect(Collectors.toList());
+    }
+
+    private GetResponse getNodesResponse() {
+        return client.prepareGet()
+            .setIndex(index)
+            .setType(ES_TYPE)
+            .setId(ES_BOOTSTRAP_DOC).setRealtime(true).setRefresh(true)
+            .execute().actionGet();
+    }
+
     private void ensureIndex() throws ExecutionException, InterruptedException {
         if (!client.admin().indices().prepareExists(index).get().isExists()) {
             client.admin().indices().prepareCreate(index).execute().get();
-            saveBootstrap(Collections.singleton(localNode));
         }
+        saveBootstrap(Collections.singleton(localNode));
     }
 
     private static String serializeNode(final Node node) {
--- logstash-core/src/test/java/org/logstash/cluster/elasticsearch/EsClientTest.java
+++ logstash-core/src/test/java/org/logstash/cluster/elasticsearch/EsClientTest.java
@@ -18,15 +18,39 @@ import org.logstash.cluster.messaging.Endpoint;
 public final class EsClientTest extends ESIntegTestCase {
 
     @Test
-    public void clusterBootstrapTest() {
+    public void clusterBootstrapTest() throws Exception {
         ensureGreen();
         final Node local = new DefaultNode(
             NodeId.from("someId"),
             new Endpoint(InetAddress.getLoopbackAddress(), TestUtils.freePort())
         );
-        try (EsClient client = new EsClient(client(), "clusterbootstraptest", local)) {
+        try (EsClient client = EsClient.create(client(), "clusterbootstraptest", local)) {
             final Collection<Node> initial = client.loadBootstrap();
             MatcherAssert.assertThat(initial, Matchers.contains(local));
+        }
+    }
+
+    @Test
+    public void multipleNodesTest() throws Exception {
+        ensureGreen();
+        final Node nodeOne = new DefaultNode(
+            NodeId.from("id-one"), new Endpoint(InetAddress.getLoopbackAddress(),
+            TestUtils.freePort())
+        );
+        final String index = "multiplenodestest";
+        try (final EsClient clientOne = EsClient.create(client(), index, nodeOne)) {
+            final Node nodeTwo = new DefaultNode(
+                NodeId.from("id-two"),
+                new Endpoint(InetAddress.getLoopbackAddress(), TestUtils.freePort())
+            );
+            try (EsClient clientTwo = EsClient.create(client(), index, nodeTwo)) {
+                MatcherAssert.assertThat(
+                    clientOne.loadBootstrap(), Matchers.contains(nodeOne, nodeTwo)
+                );
+                MatcherAssert.assertThat(
+                    clientTwo.loadBootstrap(), Matchers.contains(nodeOne, nodeTwo)
+                );
+            }
         }
     }
 }
