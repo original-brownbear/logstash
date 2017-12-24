a19127079bcd50813c90e1c8d21ac74db77771e2
Armin <me@obrown.io>
2017-12-24 08:38:09 +0100

    GA

--- logstash-core/src/test/java/org/logstash/plugins/s3input/LsS3ClusterInputTest.java
+++ logstash-core/src/test/java/org/logstash/plugins/s3input/LsS3ClusterInputTest.java
@@ -1,14 +1,27 @@
 package org.logstash.plugins.s3input;
 
+import org.elasticsearch.test.ESIntegTestCase;
+import org.junit.Assume;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 /**
  * Tests for {@link LsS3ClusterInput}.
  */
-public final class LsS3ClusterInputTest {
+public final class LsS3ClusterInputTest extends ESIntegTestCase {
+
+    private static final String TEST_BUCKET = System.getProperty("org.logstash.s3it.bucket");
+
+    private static final String TEST_KEY = System.getProperty("org.logstash.s3it.key");
+
+    private static final String TEST_SECRET = System.getProperty("org.logstash.s3it.secret");
+
+    @Rule
+    public final TemporaryFolder temporaryFolder = new TemporaryFolder();
 
     @Test
     public void readsOnMultipleNodes() {
-
+        Assume.assumeNotNull(TEST_BUCKET, TEST_KEY, TEST_SECRET);
     }
 }
