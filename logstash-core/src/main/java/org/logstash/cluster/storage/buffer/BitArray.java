516df1c72cf1f9d38959739e93f3f38131356021
Armin <me@obrown.io>
2017-12-24 16:13:28 +0100

    GA

--- logstash-core/src/main/java/org/logstash/cluster/storage/buffer/BitArray.java
+++ logstash-core/src/main/java/org/logstash/cluster/storage/buffer/BitArray.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2015-present Open Networking Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.logstash.cluster.storage.buffer;
 
 import com.google.common.base.MoreObjects;
@@ -23,13 +8,13 @@ import com.google.common.base.MoreObjects;
  * The direct bit set performs bitwise operations on a fixed count {@link UnsafeHeapBytes} instance.
  * Currently, all bytes are {@link UnsafeHeapBytes}, but theoretically {@link UnsafeMappedBytes}
  * could be used for durability as well.
- * @author <a href="http://github.com/kuujo">Jordan Halterman</a>
  */
 public class BitArray implements AutoCloseable {
 
     private final UnsafeHeapBytes bytes;
     private long size;
     private long count;
+
     private BitArray(UnsafeHeapBytes bytes, long size) {
         this(bytes, 0, size);
     }
--- logstash-core/src/main/java/org/logstash/cluster/storage/buffer/FileBytes.java
+++ logstash-core/src/main/java/org/logstash/cluster/storage/buffer/FileBytes.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2015-present Open Networking Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.logstash.cluster.storage.buffer;
 
 import java.io.File;
@@ -42,6 +27,7 @@ public class FileBytes extends AbstractBytes {
     private final String mode;
     private final RandomAccessFile randomAccessFile;
     private int size;
+
     FileBytes(File file, String mode, int size) {
         if (file == null)
             throw new NullPointerException("file cannot be null");
@@ -172,7 +158,7 @@ public class FileBytes extends AbstractBytes {
      * @param size The count of the bytes to map into memory.
      * @return The mapped bytes.
      * @throws IllegalArgumentException If {@code count} is greater than the maximum allowed
-     * {@link java.nio.MappedByteBuffer} count: {@link Integer#MAX_VALUE}
+     * {@link MappedByteBuffer} count: {@link Integer#MAX_VALUE}
      */
     public MappedBytes map(int offset, int size) {
         return map(offset, size, parseMode(mode));
@@ -185,7 +171,7 @@ public class FileBytes extends AbstractBytes {
      * @param mode The mode in which to map the randomAccessFile into memory.
      * @return The mapped bytes.
      * @throws IllegalArgumentException If {@code count} is greater than the maximum allowed
-     * {@link java.nio.MappedByteBuffer} count: {@link Integer#MAX_VALUE}
+     * {@link MappedByteBuffer} count: {@link Integer#MAX_VALUE}
      */
     public MappedBytes map(int offset, int size, FileChannel.MapMode mode) {
         MappedByteBuffer mappedByteBuffer = mapFile(randomAccessFile, offset, size, mode);
