9fe4bdc0ab07e1fe50e84bc45892bd322ea78f28
Armin <me@obrown.io>
2017-12-25 18:43:49 +0100

    GA

--- logstash-core/src/main/java/org/logstash/cluster/protocols/raft/protocol/CloseSessionResponse.java
+++ logstash-core/src/main/java/org/logstash/cluster/protocols/raft/protocol/CloseSessionResponse.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2017-present Open Networking Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.logstash.cluster.protocols.raft.protocol;
 
 import java.util.Objects;
@@ -23,7 +8,7 @@ import org.logstash.cluster.protocols.raft.RaftError;
  */
 public class CloseSessionResponse extends SessionResponse {
 
-    public CloseSessionResponse(Status status, RaftError error) {
+    public CloseSessionResponse(RaftResponse.Status status, RaftError error) {
         super(status, error);
     }
 
@@ -31,8 +16,8 @@ public class CloseSessionResponse extends SessionResponse {
      * Returns a new keep alive response builder.
      * @return A new keep alive response builder.
      */
-    public static Builder builder() {
-        return new Builder();
+    public static CloseSessionResponse.Builder builder() {
+        return new CloseSessionResponse.Builder();
     }
 
     @Override
@@ -47,7 +32,7 @@ public class CloseSessionResponse extends SessionResponse {
     /**
      * Status response builder.
      */
-    public static class Builder extends SessionResponse.Builder<Builder, CloseSessionResponse> {
+    public static class Builder extends SessionResponse.Builder<CloseSessionResponse.Builder, CloseSessionResponse> {
         @Override
         public CloseSessionResponse build() {
             validate();
